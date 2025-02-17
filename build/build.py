63aa90aa6b3eaa1c62da97af096ccda437c50e3b
MIRIMIRIM <59959583+MIRIMIRIM@users.noreply.github.com>
2025-02-17 23:32:17 +0800

    add shared pkgs

---
+++ build/build.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+import sys
+import subprocess
+
+scripts_path = "../scripts/"
+project_file = f"{scripts_path}project.ini"
+zig_conf_file = f"{scripts_path}zig.ini"
+build_dir = "../build/{runtime}-{lib_type}"
+runtime_conf = "{scripts_path}{runtime}.ini"
+
+native_runtime = ['win-x64', 'osx-arm64']
+
+architectures = ["x64", "arm64"]
+lib_types = ["static", "shared"]
+runtime_template = "{prefix}-{arch}"
+platforms = {
+    "win": {
+        "prefix": "win",
+        "args": ["-Db_vscrt=static_from_buildtype"]
+    },
+    "linux": {
+        "prefix": "linux-musl",
+        "args": ["--native-file", zig_conf_file, "--cross-file", zig_conf_file]
+    },
+    "mac": {
+        "prefix": "osx",
+        "args": []
+    }
+}
+
+runtimes = [runtime_template.format(prefix=platforms[p]["prefix"], arch=arch) for p in platforms for arch in architectures]
+
+def get_configure_command_args(runtime, lib_type):
+
+    runtime_splt = runtime.split('-')
+    platform, arch = runtime_splt[0], runtime_splt[-1]
+    special_args = [p['args'] for p in platforms.values() if p['prefix'] == platform][0]
+
+    args = ["meson"]
+
+    if len(special_args) != 0:
+        args += special_args
+
+    if runtime in native_runtime:
+        args += ['--native-file']
+    else:
+        args += ['--cross-file']
+    args += [project_file]
+
+    if runtime not in native_runtime:
+        args += ['--cross-file', runtime_conf.format(scripts_path = scripts_path, runtime = runtime)]
+
+    if lib_type == 'static':
+        args += ['-Ddefault_library=static']
+
+    args += [build_dir.format(runtime = runtime, lib_type = lib_type)]
+    
+    return args
+
+def execute(runtime, lib_type):
+    configure_args = get_configure_command_args(runtime, lib_type)
+    subprocess.run(configure_args)
+
+    compile_command_args = ["meson", "compile", "-C", configure_args[-1]]
+    subprocess.run(compile_command_args)
+
+def main():
+    if len(sys.argv) != 3:
+        print("usage: {} <runtime> <lib_type>".format(sys.argv[0]))
+        print(f"runtime options: {', '.join(runtimes)}")
+        print("lib_type options: static, shared")
+        sys.exit(1)
+    
+    runtime_arg = sys.argv[1].lower()
+    lib_type_arg = sys.argv[2].lower()
+
+    if runtime_arg not in runtimes:
+        print(f"Unsupport platform: {runtime_arg}. Optional value: {', '.join(runtimes)}")
+        sys.exit(1)
+    if lib_type_arg not in lib_types and lib_type_arg != 'all':
+        print(f"Unsupport lib_type: {lib_type_arg}. Optional value: all, {', '.join(lib_types)}")
+        sys.exit(1)
+    
+    if lib_type_arg == 'all':
+        for t in lib_types:
+            execute(runtime_arg, t)
+    else:
+        execute(runtime_arg, lib_type_arg)
+
+if __name__ == "__main__":
+    main()
---
+++ build/gen-csproj.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+import sys
+
+base_lib_name = "libharfbuzz"
+architectures = ["x64", "arm64"]
+lib_types = ["static", "shared"]
+runtime_template = "{prefix}-{arch}"
+platforms = {
+    "win": {
+        "prefix": "win",
+    },
+    "linux": {
+        "prefix": "linux-musl",
+    },
+    "mac": {
+        "prefix": "osx",
+    }
+}
+
+extension_map = {
+    "win": {"static": ".a", "shared": ".dll"},
+    "linux": {"static": ".a", "shared": ".so"},
+    "mac": {"static": ".a", "shared": ".dylib"}
+}
+
+def generate_csproj_content(platform_key, lib_type):
+    platform_info = platforms[platform_key]
+    package_id = f"MIR.NativeLib.Harfbuzz.{lib_type.capitalize()}.{platform_key.capitalize()}"
+    file_ext = extension_map[platform_key][lib_type]
+    
+    none_entries = []
+    for arch in architectures:
+        runtime_dir = runtime_template.format(
+            prefix=platform_info["prefix"],
+            arch=arch,
+        )
+        runtime_bt_dir = runtime_dir + '-' + lib_type
+
+        for lib in ["", "-subset"]:
+            entry = (
+                f'    <None Include="{runtime_bt_dir}\\src\\{base_lib_name}{lib}{file_ext}" Pack="true" PackagePath="runtimes\\{runtime_dir}\\native" />'
+            )
+            none_entries.append(entry)
+    
+    none_items = "\n".join(none_entries)
+    
+    csproj_content = f'''<Project Sdk="Microsoft.NET.Sdk">
+  <PropertyGroup>
+    <PackageId>{package_id}</PackageId>
+  </PropertyGroup>
+  <ItemGroup>
+{none_items}
+  </ItemGroup>
+  <Import Project="global.props" />
+</Project>
+'''
+    return package_id, csproj_content
+
+def gen_csproj(platform):
+    for lib_type in lib_types:
+        package_id, content = generate_csproj_content(platform, lib_type)
+        filename = f"{package_id}.csproj"
+        with open(filename, "w", encoding="utf-8") as f:
+            f.write(content)
+        print(f"Generate '{filename}'")
+
+def main():
+    if len(sys.argv) != 2:
+        print("usage: {} <platform>".format(sys.argv[0]))
+        print("platform options: win, linux, mac")
+        sys.exit(1)
+        
+    platform_arg = sys.argv[1].lower()
+    
+    if platform_arg not in platforms and platform_arg != 'all':
+        print(f"Unsupport platform: {platform_arg}. Optional value: {', '.join(platforms.keys())}")
+        sys.exit(1)
+    
+    if platform_arg == 'all':
+        for plat in platforms.keys():
+            gen_csproj(plat)
+    else:
+        gen_csproj(platform_arg)
+
+if __name__ == "__main__":
+    main()
